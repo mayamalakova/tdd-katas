1a494c63ecc368aabbf3895e32165b656b9557c9
Maya Malakova <maya.malakova@red-gate.com>
2017-08-04 15:21:16 +0100

    Condense test cases for winning hand

--- tests/Tdd.Exercise7.Tests/RoundShould.cs
+++ tests/Tdd.Exercise7.Tests/RoundShould.cs
@@ -20,38 +20,15 @@ namespace Tdd.Exercise7.Tests
             player2 = Substitute.For<IPlayer>();
         }
 
-        [TestCase(1, 2)]
-        [TestCase(2, 4)]
-        public void One_times_two_is_two(int multiplier, int expected)
-        {
-            var result = multiplier * 2;
-            result.ShouldBe(expected);
-        }
-
-       [TestCase(Hand.Paper, Hand.Scissors, Winner.Player2)]
-        public void Decide_scissors_beats_paper(Hand hand1, Hand hand2, Winner expectedWinner)
+        [TestCase(Hand.Paper, Hand.Scissors, Winner.Player2)]
+        [TestCase(Hand.Paper, Hand.Rock, Winner.Player1)]
+        [TestCase(Hand.Rock, Hand.Scissors, Winner.Player1)]
+        public void Decide_winning_hand(Hand hand1, Hand hand2, Winner expectedWinner)
         {
             player1.RevealHand().Returns(hand1);
             player2.RevealHand().Returns(hand2);
             var winner = gameRound.Play(player1, player2);
             winner.ShouldBe(expectedWinner);
-        }
-        [Test]
-        public void Decide_paper_beats_rock()
-        {
-            player1.RevealHand().Returns(Hand.Paper);
-            player2.RevealHand().Returns(Hand.Rock);
-            var winner = gameRound.Play(player1, player2);
-            winner.ShouldBe(Winner.Player1);
-        }
-
-        [Test]
-        public void Decide_rock_beats_scissors()
-        {
-            player1.RevealHand().Returns(Hand.Rock);
-            player2.RevealHand().Returns(Hand.Scissors);
-            var winner = gameRound.Play(player1, player2);
-            winner.ShouldBe(Winner.Player1);
         }
 
         [Test]
