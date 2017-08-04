81bae7f34249b7a84813e5db28c51b4241147d40
Maya Malakova <maya.malakova@red-gate.com>
2017-08-04 15:58:52 +0100

    Move stuff into a static class

--- src/Tdd.Exercise7/GameRound.cs
+++ src/Tdd.Exercise7/GameRound.cs
@@ -4,13 +4,6 @@ namespace Tdd.Exercise7
 {
     public class GameRound
     {
-        private static readonly Dictionary<Hand, Hand> WinningHand = new Dictionary<Hand, Hand>
-        {
-            {Hand.Scissors, Hand.Paper},
-            {Hand.Paper, Hand.Rock},
-            {Hand.Rock, Hand.Scissors},
-        };
-
         public Winner Play(IPlayer player1, IPlayer player2)
         {
             var hand1 = player1.RevealHand();
@@ -21,17 +14,12 @@ namespace Tdd.Exercise7
                 return Winner.None;
             }
 
-            if (IsWinningHand(hand1, hand2))
+            if (GameRoundUtils.IsWinningHand(hand1, hand2))
             {
                 return Winner.Player1;
             }
 
             return Winner.Player2;
-        }
-
-        private static bool IsWinningHand(Hand you, Hand competition)
-        {
-            return WinningHand[you] == competition;
         }
     }
 }
---
+++ src/Tdd.Exercise7/GameRoundUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Tdd.Exercise7
+{
+    public class GameRoundUtils
+    {
+        private static readonly Dictionary<Hand, Hand> WinningHand = new Dictionary<Hand, Hand>
+        {
+            {Hand.Scissors, Hand.Paper},
+            {Hand.Paper, Hand.Rock},
+            {Hand.Rock, Hand.Scissors},
+        };
+
+        public static bool IsWinningHand(Hand you, Hand competition)
+        {
+            return WinningHand[you] == competition;
+        }
+    }
+}
