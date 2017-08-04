20fb6c4b91970f659175ea8535e91d69444b1d6d
Maya Malakova <maya.malakova@red-gate.com>
2017-08-04 15:08:44 +0100

    Adapt to our new field naming convention

--- tests/Tdd.Exercise7.Tests/RoundShould.cs
+++ tests/Tdd.Exercise7.Tests/RoundShould.cs
@@ -8,50 +8,50 @@ namespace Tdd.Exercise7.Tests
     [TestFixture]
     public class RoundShould
     {
-        private GameRound _gameRound;
-        private IPlayer _player1;
-        private IPlayer _player2;
+        private GameRound gameRound;
+        private IPlayer player1;
+        private IPlayer player2;
 
         [SetUp]
         public void SetUp()
         {
-            _gameRound = new GameRound();
-            _player1 = Substitute.For<IPlayer>();
-            _player2 = Substitute.For<IPlayer>();
+            gameRound = new GameRound();
+            player1 = Substitute.For<IPlayer>();
+            player2 = Substitute.For<IPlayer>();
         }
 
         [Test]
         public void Decide_scissors_beats_paper()
         {
-            _player1.RevealHand().Returns(Hand.Paper);
-            _player2.RevealHand().Returns(Hand.Scissors);
-            var winner = _gameRound.Play(_player1, _player2);
+            player1.RevealHand().Returns(Hand.Paper);
+            player2.RevealHand().Returns(Hand.Scissors);
+            var winner = gameRound.Play(player1, player2);
             winner.ShouldBe(Winner.Player2);
         }
         [Test]
         public void Decide_paper_beats_rock()
         {
-            _player1.RevealHand().Returns(Hand.Paper);
-            _player2.RevealHand().Returns(Hand.Rock);
-            var winner = _gameRound.Play(_player1, _player2);
+            player1.RevealHand().Returns(Hand.Paper);
+            player2.RevealHand().Returns(Hand.Rock);
+            var winner = gameRound.Play(player1, player2);
             winner.ShouldBe(Winner.Player1);
         }
 
         [Test]
         public void Decide_rock_beats_scissors()
         {
-            _player1.RevealHand().Returns(Hand.Rock);
-            _player2.RevealHand().Returns(Hand.Scissors);
-            var winner = _gameRound.Play(_player1, _player2);
+            player1.RevealHand().Returns(Hand.Rock);
+            player2.RevealHand().Returns(Hand.Scissors);
+            var winner = gameRound.Play(player1, player2);
             winner.ShouldBe(Winner.Player1);
         }
 
         [Test]
         public void Decide_same_hands_are_a_draw()
         {
-            _player1.RevealHand().Returns(Hand.Rock);
-            _player2.RevealHand().Returns(Hand.Rock);
-            var winner = _gameRound.Play(_player1, _player2);
+            player1.RevealHand().Returns(Hand.Rock);
+            player2.RevealHand().Returns(Hand.Rock);
+            var winner = gameRound.Play(player1, player2);
             winner.ShouldBe(Winner.None);
         }
     }
